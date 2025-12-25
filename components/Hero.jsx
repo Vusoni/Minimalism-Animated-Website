2f8771c7f6b035095ccdc80994d6d6d205745359
Vusoni <valoenu.contact@gmail.com>
2025-12-25 21:45:55 +0100

    First day Project finished - Before Styling.

---
+++ components/Hero.jsx
@@ -0,0 +1,133 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
+
+// Hero Section
+const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  // TODO GSAP SECTION
+  // useGsap hook
+  useGSAP(() => {
+    // Split letters to show in animation state
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
+
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    // Apply text-gradient class once before animating
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out", // Smooth feeling
+      stagger: 0.07,
+    });
+
+    // Split Text
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
+
+    // First property refer to the element I animating
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    z;
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
+  }, []);
+  c;
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MINIMALISM</h1>
+
+        {/* Leafs Images */}
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Sophistic. Classic. Elegance.</p>
+              <p className="subtitle">
+                Develop new environment <br /> of Paris
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every coffee on our menu is a blend of premium ingredients,
+                subtle craft, and timeless recipes—designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+          src="/videos/output.mp4"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Hero;
